c9ac641a076318d3b93560c98605e4079e3dfd5d
sudesh18 <sudesh18iitg@gmail.com>
2020-10-17 14:58:17 +0530

    add files for the application

---
+++ wine/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from wine.views import *
+
+urlpatterns = [
+    path('', WineList),
+    path("wine_listing/", WineListing.as_view(), name = 'listing'),
+    path("ajax/countries/", getCountries, name = 'get_countries'),
+    path("ajax/variety/", getvariety, name = 'get_varieties'),
+    path("ajax/province/", getProvince, name = 'get_provinces'),
+    path("ajax/region/", getRegion, name = 'get_regions'),
+]
